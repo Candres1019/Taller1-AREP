935c79d6959d5c719c88177a6b0b8b430fb2a35a
Andres Calderon <mateo.cor.amco@gmail.com>
2021-01-22 18:29:51 -0500

    Avance Implementacion LinkedList

--- src/main/java/edu/escuelaing/arep/App.java
+++ src/main/java/edu/escuelaing/arep/App.java
@@ -6,8 +6,17 @@ package edu.escuelaing.arep;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        LinkedListImp linkedListImp = new LinkedListImp();
+        linkedListImp.addLast(4.5);
+        linkedListImp.addLast(6.0);
+        linkedListImp.addLast(3.9);
+        linkedListImp.addLast(1.9);
+        linkedListImp.addLast(9.3);
+
+        linkedListImp.add(4, 14.5);
+
+        String printStr = linkedListImp.linkedListToString();
+        System.out.println(printStr);
     }
 }
---
+++ src/main/java/edu/escuelaing/arep/CalculadoraEstadistica.java
@@ -0,0 +1,6 @@
+package edu.escuelaing.arep;
+
+import java.util.List;
+
+public class CalculadoraEstadistica {
+}
